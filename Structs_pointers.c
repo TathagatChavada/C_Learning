5f57bc795366e1fc736b1e7c991b98a457be2a7a
Tathagat <chavadatathagat1234@gmail.com>
2023-10-17 22:59:14 +0530

    Files added

--- Structs_pointers.c
+++ Structs_pointers.c
@@ -6,6 +6,7 @@ typedef struct PersonDetail
 {
     int age;
     int weight;
+
 }personDetail;
 
 
@@ -17,49 +18,49 @@ typedef struct personInfo
 }person;
 
 
-// int main()
-// {
-//     personDetail *personPtr, person1;
-//     personPtr = &person1;     //personPtr is pointer to some variable of type struct Person
+int main()
+{
+    personDetail *personPtr, person1;
+    personPtr = &person1;     //personPtr is pointer to some variable of type struct Person
 
-//     printf("Enter your age: ");
-//     scanf("%d", &(personPtr->age));
-//     printf("\n");
+    printf("Enter your age: ");
+    scanf("%d", &(personPtr->age));
+    printf("\n");
 
-//     printf("Enter your weight: ");
-//     scanf("%d", &(personPtr->weight));
-//     printf("\n");
+    printf("Enter your weight: ");
+    scanf("%d", &(personPtr->weight));
+    printf("\n");
 
-//     // personPtr->age == (*personPtr).age
+    // personPtr->age == (*personPtr).age
  
-//     printf("Age: %d\n", personPtr->age);
-//     printf("Weight: %d", personPtr->weight);
+    printf("Age: %d\n", personPtr->age);
+    printf("Weight: %d", personPtr->weight);
 
 
-//     return 0;
+    return 0;
 
-// }
-
-
-int main() 
-{ 
-    person *ptr;
-    int i, n;
-
-    printf("Enter the number of Person: ");
-    scanf("%d", &n);
-
-    ptr = (person* ) malloc(n * sizeof(person));
-
-    for (i=0; i<n; i++){
-
-        printf("Enter Name & Age respectively: ");
-        scanf("%s %d", (ptr + i)->name, &(ptr + i)->age);
-
-    }
-
-    printf("Displaying Information: \n");
-    for (i=0; i<n; i++){
-        printf("Name: %s \t Age: %d \n", (ptr + i)->name, (ptr + i)->age);
-    }
 }
+
+
+// int main() 
+// { 
+//     person *ptr;
+//     int i, n;
+
+//     printf("Enter the number of Person: ");
+//     scanf("%d", &n);
+
+//     ptr = (person* ) malloc(n * sizeof(person));
+
+//     for (i=0; i<n; i++){
+
+//         printf("Enter Name & Age respectively: ");
+//         scanf("%s %d", (ptr + i)->name, &(ptr + i)->age);
+
+//     }
+
+//     printf("Displaying Information: \n");
+//     for (i=0; i<n; i++){
+//         printf("Name: %s \t Age: %d \n", (ptr + i)->name, (ptr + i)->age);
+//     }
+// }
